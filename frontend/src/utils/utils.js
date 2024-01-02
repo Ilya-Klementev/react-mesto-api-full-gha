3f1dac3a54ebe5303699e063163e464960ee17d2
Ilya-Klementev <klementevvv@yandex.ru>
2024-01-02 18:01:15 +0100

    спринт 15, дkz загрузки на сервер 10

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,7 +1,3 @@
-// const adressServer = 'https://mesto.nomoreparties.co/v1/cohort-70';
-//const token = 'fdc38e01-d1b8-42b2-bc1e-20ad07d33697';
-// const addressAuth = 'https://auth.nomoreparties.co';
-
 const adressServer = 'http://api.mesto.nomoredomainsmonster.ru';
 const addressAuth = 'http://api.mesto.nomoredomainsmonster.ru';
 
