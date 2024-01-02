cc2b96f0133e42ca99ff22d7f71454b5b2b02ec9
Ilya-Klementev <klementevvv@yandex.ru>
2024-01-02 20:28:04 +0100

    спринт 15, для загрузки на сервер 14

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,8 +1,4 @@
-// const adressServer = 'https://mesto.nomoreparties.co/v1/cohort-70';
-//const token = 'fdc38e01-d1b8-42b2-bc1e-20ad07d33697';
-// const addressAuth = 'https://auth.nomoreparties.co';
-
-const adressServer = 'http://localhost:3000';
-const addressAuth = 'http://localhost:3000';
+const adressServer = 'http://mesto.klementev.nomoredomainsmonster.ru/api/';
+const addressAuth = 'http://mesto.klementev.nomoredomainsmonster.ru/api/';
 
 export {  adressServer, addressAuth };
