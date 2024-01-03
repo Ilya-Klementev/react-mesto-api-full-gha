1baa4759e85365459d8a7ae0e7d1756515e92113
Ilya-Klementev <klementevvv@yandex.ru>
2024-01-03 02:41:58 +0100

    спринт 15, дkz загрузки на сервер 27

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -6,11 +6,13 @@ const cors = (req, res, next) => {
     /(https|http)?:\/\/(?:www\.|(?!www))mesto.klementev.nomoredomainsmonster.ru\/[a-z]+\/|[a-z]+\/|[a-z]+(\/|)/,
   ];
 
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+  // const { origin } = req.headers;
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  //   res.header('Access-Control-Allow-Credentials', true);
+  // }
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
